3fd42db09c4fae3b3139b6078612639bc01373a9
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-03-26 13:23:20 +0100

    Exo 2/index.html Exo 2/script.js

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,64 @@
 // you can write js here
 console.log('exo-2');
+
+let isTesting = false;
+if ( isTesting === false) {
+    
+
+
+const myDate = new Date();
+
+var dateWeek = 'Pas encore le weekend :(';
+var dateWeekend = 'On est en weekend !';
+
+
+var dateActual = myDate.getDay(); 
+
+var hourActual = myDate.getHours();
+
+
+
+
+    console.log(dateActual);
+    if(dateActual < 6 && dateActual < 7 ) {
+
+        console.log(dateWeek);
+
+    }
+    else {
+        console.log(dateWeekend)
+    }
+console.log(hourActual);
+    if(hourActual < 17 ) {
+        console.log(dateWeek);
+    }
+    else {
+        console.log(dateWeekeed);
+    }
+}
+isTesting = true;
+if (isTesting === true) {
+   
+
+    var dateWeek = 'Pas encore le weekend :(';
+    var dateWeekend = 'On est en weekend !';
+    
+    
+    var dateActual = 26; 
+    
+    var hourActual = 14 ; 
+    console.log(dateActual);
+    if(dateActual < 27 && dateActual < 28 ) {
+        console.log(dateWeek);
+    }
+    else {
+        console.log(dateWeekend)
+    }
+console.log(hourActual);
+    if(hourActual < 17 ) {
+        console.log(dateWeek);
+    }
+    else {
+        console.log(dateWeekeed);
+    }
+}
