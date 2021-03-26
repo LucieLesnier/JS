78a0ef4d380d82417de29a64b69fb162a74c4fd3
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-03-26 10:47:42 +0100

    Exo 0/index.html, Exo 1/index.html, Exo 1/script.js

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,12 @@
 // you can write js here
 console.log('hello from file');
+var kelvins=prompt("Quelle est la température en Kelvins aujourd'hui ?");
+//var kelvins=294;
+var celsius= kelvins - 273; //Conversion de kelvins en celsius
+console.log(celsius);
+
+var fahrenheits = celsius * (9/5) + 32; // Nous reprenons la variable celsius et nous calculons sa conversion en fahrenheits
+console.log(fahrenheits);
+
+console.log(Math.floor(fahrenheits))
+
