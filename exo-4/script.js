a75dac93251369d1361ff8cf94c12768a213b835
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-03-29 10:58:27 +0200

    Exo-4/ index.html, Exo-4/script.js

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,18 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+
+var last = secretMessage.pop();
+
+
+var add = secretMessage.push('to', 'program');
+var change = secretMessage.indexOf('easily'); 
+var removeIndex = secretMessage.splice(change, 1);
+var addIndex = secretMessage.splice(6, 1, 'right');
+var deleteFirst = secretMessage.shift();
+var addFirst = secretMessage.unshift ('Programming');
+var removeMany = secretMessage.splice(5, 4, 'know');
+
+
+console.log(secretMessage.join());
