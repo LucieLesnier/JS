d36a85194f021bd0657a6fba0c46cebac351f889
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-03-29 11:57:06 +0200

    Exo-5/ index.html, Exo-5/script.js

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,27 @@
 // you can write js here
 
 console.log('exo-5');
+
+input = 'Hello World';
+
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+resultArray = [];
+
+for (i = 0; i < input.length; i ++ ) {
+    //console.log(input[i]);
+    for (j = 0; j < vowels.length; j ++ ) {
+        if (input[i] == vowels[j] ) {
+
+resultArray.push(input[i]);
+
+console.log(resultArray.join().toUpperCase());
+        }
+    }
+}
+
+for (i = 0; i < input.length; i ++ ) {
+    console.log(input.indexOf(vowels[i]));
+    
+}
